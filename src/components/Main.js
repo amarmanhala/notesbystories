af883ea627d876495930d39410d37afbab678029
amarmanhala <amar.manhala@gmail.com>
2022-03-23 01:43:42 -0400

    Changes

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,10 +4,10 @@ import Header from "./Header";
 
 function Main() {
   return (
-    <>
+    <div>
       <Header></Header>
       <Editor></Editor>
-    </>
+    </div>
   );
 }
 export default Main;
