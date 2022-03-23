bf445a72a23cd0fe027d30f966ee3d6fb5e27fe2
amarmanhala <amar.manhala@gmail.com>
2022-03-22 22:47:12 -0400

    Minor Change

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import { Routes, Route, Link } from "react-router-dom";
 import GlobalStyles from "./GlobalStyles";
 import "./index.css";
-import Header from "./components/Header";
-import Editor from "./components/Editor";
 import Welcome from "./components/Welcome";
 import Main from "./components/Main";
 
